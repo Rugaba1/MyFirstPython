b3fdfc3cae41b3fd681c1dceea11b88f9a928b6a
Rugaba Innocent <gilbertkendrick9@gmail.com>
2024-02-02 00:07:00 -0800

    third

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+a = int(input("Enter The First Number : "))
+b = int(input("Enter The Second Number : "))
+sums = a+b
+product = a*b
+div = a/b
+
+print(a ,"+", b ," = ", sums)
+print(a ,"*", b ," = ", product)
+print(a ,"/", b ," = ", div)
--- second.py
+++ second.py
@@ -115,12 +115,94 @@
 # a = input("Enter a number: ")
 # print("You entered: ",a)
 
-a = int(input("Enter a initial number: "))
-b = int(input("Enter a Final number: "))
+# a = int(input("Enter a initial number: "))
+# b = int(input("Enter a Final number: "))
 
 
-for i in range(a,b):
-    if i%2==0:
-        print(i)
-    else:
-        continue
+# for i in range(a,b):
+#     if i%2==0:
+#         print(i)
+#     else:
+#         continue
+
+# thislist = ["apple","banana","cherry"]
+# thislist[1:3]=["watermelon"]
+# print(thislist)
+
+# thislist = ["apple","banana","cherry"]
+# thislist.insert(2,"watermelon")
+# print(thislist)
+
+# thislist = ["apple","banana","cherry"]
+# thislist.append("Orange")
+# print(thislist)
+
+# thislist = ["apple","banana","cherry"]
+# tropical = ("mango","pineapple","papaya")
+# thislist.extend(tropical)
+# print(thislist)
+
+# thislist = ["apple","banana","cherry"]
+# thislist.remove("banana")
+# print(thislist)
+
+# thislist = ["apple","banana","cherry"]
+# thislist.pop(1)
+# print(thislist)
+
+# thislist = ["apple","banana","cherry"]
+# del thislist[0]
+# print(thislist)
+
+# thislist = ["apple","banana","cherry"]
+# thislist.clear()
+# print(thislist)
+
+# thislist = ["apple","banana","cherry"]
+
+# for x in thislist:
+#     print(x)
+
+# thislist = ["apple","banana","cherry"]
+
+# for x in range(len(thislist)):
+#     print(thislist[x])
+
+# thislist = ["apple","banana","cherry"]
+# i = 0
+
+# while i<len(thislist):
+#     print(thislist[i])
+#     i = i+1
+
+# thislist = ["apple","banana","cherry"]
+# [print(x) for x in thislist]
+
+# fruit = ["apple","banana","cherry","kiwi","mango"]
+# newlist = []
+
+# for x in fruit:
+#     if "i" in x:
+#         newlist.append(x)
+# print(newlist)
+
+# thislist = ["apple","banana","cherry","kiwi","mango"]
+# thislist.sort()
+# print(thislist)
+
+# def myfunc(n):
+#     return abs(n-50)
+# thislist = [100,50,65,82,23]
+# thislist.sort(key = myfunc)
+# print(thislist)
+
+# thislist = ["apple","banana","cherry","kiwi","mango"]
+# thislist.reverse()
+# print(thislist)
+
+list1  = ["a","b","c"]
+list2 = [1,2,3]
+
+for x in list2:
+    list1.append(x)
+print(list1)
